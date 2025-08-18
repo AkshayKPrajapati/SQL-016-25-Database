297dc27c30dda7ebafa984e3fc2491bb59b810eb
AkshayKPrajapati <akshayprajapati6200@outlook.com>
2025-08-18 19:26:17 +0530

    alter

--- batch01625.sql
+++ batch01625.sql
@@ -31,4 +31,51 @@ ALTER TABLE customer MODIFY age INT;
 
 show tables;
 select * from customer;
- 
+
+
+-- create location table
+
+create table location(
+    location_id int primary key,
+    city varchar(50),
+    pincode int(8)
+);
+
+
+ -- create department table 
+ create table department(
+dept_id int primary key auto_increment,
+dept_name varchar(20) not null,
+loc_id int ,
+constraint dept_location_fk foreign key(loc_id) references location(location_id)
+);
+
+select * from department;
+-- create table of the employee
+
+create table employee(
+emp_id int primary key auto_increment,
+emp_name varchar(20) not null,
+salary int,
+dept_id int not null,
+constraint dept_id_fk foreign key(dept_id) references department(dept_id),
+manager_id int ,
+constraint manager_id_fk foreign key(manager_id) references employee(emp_id)
+);
+select * from employee;
+
+-- use of the alter (change something)
+alter table student add email varchar (20);
+
+-- add contact on the employee table
+alter table employee add contact bigint(10);
+
+-- delete the email column on the student table
+alter table student drop email; 
+
+-- modefie the datatype
+alter table student modify contact char(10);
+
+-- change the name of the student table 
+alter table student rename column sname to student_name;
+
---
+++ department_table.sql
@@ -0,0 +1,7 @@
+use batch01625;
+
+
+create table department(
+    department_id int primary key,
+    name varchar(20)
+);
