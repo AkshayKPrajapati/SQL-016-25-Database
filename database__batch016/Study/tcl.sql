b215e57d64736fed5bdff262ea45cab72f71798a
AkshayKPrajapati <akshayprajapati6200@outlook.com>
2025-08-19 11:49:22 +0530

    transaction contol language

--- database__batch016/Study/tcl.sql
+++ database__batch016/Study/tcl.sql
@@ -12,5 +12,18 @@ commit;
 start transaction;
 select * from location;
 insert into location (location_id,city,pincode) 
-value (4,'ranchi',822021);
+value (5,'NGP',222721);
 rollback;
+
+start transaction;
+select * from location;
+insert into location (location_id,city,pincode) 
+value (5,'NGP',222721);
+insert into location (location_id,city,pincode) 
+value (6,'Nashik',852721);
+select * from location;
+savepoint s1;
+delete from location where location_id = 6;
+select * from location;
+rollback to s1;
+select * from location;
